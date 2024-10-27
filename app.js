12881700d6f2e8d7906a5e00bdb7d0f5e8935bc2
Harry <105970544+h-s14@users.noreply.github.com>
2024-10-27 11:03:38 +0530

    -bare-deploy-

--- app.js
+++ app.js
@@ -14,11 +14,7 @@ config({ path: ".env" });
 
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL,
-      process.env.DASHBOARD_URL,
-      process.env.FRONTEND_IP,
-    ],
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
